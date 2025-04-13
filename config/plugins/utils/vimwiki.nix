48e51545e723fe442447d41d72f660a99fc7a070
immortalrover <immortalrover@protonmail.com>
2025-04-13 15:59:09 +0800

    test_07

--- config/plugins/utils/vimwiki.nix
+++ config/plugins/utils/vimwiki.nix
@@ -1,26 +1,27 @@
 {
-  plugins.vimwiki = {
-    enable = true;
-    # settings = {
-    #   autowriteall = 0;
-    #   global_ext = 0;
-    #   hl_cb_checked = 1;
-    #   hl_headers = 1;
-    #   key_mappings = {
-    #     all_maps = 1;
-    #     global = 1;
-    #     headers = 1;
-    #   };
-    #   list = [
-    #     {
-    #       ext = ".md";
-    #       path = "~/docs/notes/";
-    #       syntax = "markdown";
-    #     }
-    #   ];
-    #   listsym_rejected = "✗";
-    #   listsyms = "○◐●✓";
-    #   use_calendar = 1;
-    # };
-  };
+  plugins.vimwiki.enable = true;
+  # plugins.vimwiki = {
+  #   enable = true;
+  #   # settings = {
+  #   #   autowriteall = 0;
+  #   #   global_ext = 0;
+  #   #   hl_cb_checked = 1;
+  #   #   hl_headers = 1;
+  #   #   key_mappings = {
+  #   #     all_maps = 1;
+  #   #     global = 1;
+  #   #     headers = 1;
+  #   #   };
+  #   #   list = [
+  #   #     {
+  #   #       ext = ".md";
+  #   #       path = "~/docs/notes/";
+  #   #       syntax = "markdown";
+  #   #     }
+  #   #   ];
+  #   #   listsym_rejected = "✗";
+  #   #   listsyms = "○◐●✓";
+  #   #   use_calendar = 1;
+  #   # };
+  # };
 }
