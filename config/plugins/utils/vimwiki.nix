3a9c66db97711868278033139fc5e8ff93e50fed
immortalrover <immortalrover@protonmail.com>
2025-07-13 15:08:26 +0800

    rewrite vimwiki plugin

--- config/plugins/utils/vimwiki.nix
+++ config/plugins/utils/vimwiki.nix
@@ -1,12 +1,25 @@
 { pkgs, ... }:
+let
+  vimwiki = pkgs.vimUtils.buildVimPlugin {
+    name = "vimwiki";
+    src = pkgs.fetchFromGitHub {
+      owner = "immortalrover";
+      repo = "vimwiki";
+      rev = "ea6b99b95f8d08f782bc4bdec77fbec6dffebad5";
+      hash = "sha256-sSpbzu3BPYgf27m/pflEnxjniPIqLEOWP4cYajsHPoo=";
+    };
+  };
+in
 {
   plugins.vimwiki = {
     enable = true;
-    package = pkgs.vimPlugins.vimwiki;
+    package = vimwiki;
     settings = {
+      auto_header = 1;
       list = [
         {
           ext = ".md";
+          path = "~/.data/";
         }
       ];
     };
